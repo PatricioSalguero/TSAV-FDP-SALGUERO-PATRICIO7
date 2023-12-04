9e608c79ab7c6c60442c9bb443da1c619ba761ab
Patricio Salguero <Patricio.salguero@estduiante.imagecampus.edu.ar>
2023-12-04 12:31:50 -0300

    Condiciones Aanzadas

---
+++ Assets/Scripts/Tumba.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tumba : MonoBehaviour
+{
+   public void habla()
+   {
+        Debug.Log("Estoy descansando en mi TUMBA");
+   }
+}
---
+++ Packages/Assets/Scripts/Raycast.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+           Tumba muerto = hit.collider.gameObject.GetComponent<Tumba>();
+
+            if (muerto != null) 
+            {
+                    muerto.habla();
+            }
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+        }
+
+
+}
